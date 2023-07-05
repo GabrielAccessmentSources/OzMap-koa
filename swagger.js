0cbb7d2c68dfdbb98e17ab0fc39fe339d4c5496e
gabriel savian <gabrielsvnzrd@gmail.com>
2023-07-05 19:01:03 -0300

    feat: added swagger configs for the getUsers method

--- swagger.js
+++ swagger.js
@@ -1,10 +1,9 @@
 import Router from "koa-router";
 import swaggerJSDoc from "swagger-jsdoc";
-import swaggerUIKoa from "swagger-ui-koa";
+import swaggerUI from "swagger-ui-koa";
 
 const router = new Router();
 
-// Swagger configuration options
 const options = {
     definition: {
         openapi: "3.0.0",
@@ -14,22 +13,15 @@ const options = {
             description: "API documentation for Your API",
         },
     },
-    // Path to the API docs
     apis: ["./src/routes/*.js"],
 };
 
-// Initialize swagger-jsdoc
 const swaggerSpec = swaggerJSDoc(options);
 
-// Serve swagger API docs
 router.get("/api-docs.json", async (ctx) => {
     ctx.body = swaggerSpec;
 });
 
-// Serve swagger UI
-router.get("/api-docs", swaggerUIKoa(), async (ctx) => {
-    // Redirect to the Swagger UI
-    ctx.redirect("/api-docs/index.html");
-});
+router.get("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 export default router;
